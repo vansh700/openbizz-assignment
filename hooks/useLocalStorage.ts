cb08e855e92d77465c9f560ca3158384d3a117cd
Vansh <vv623106@gmail.com>
2026-07-01 09:53:38 +0530

    add useLocalStorage custom

--- hooks/useLocalStorage.ts
+++ hooks/useLocalStorage.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 export function useLocalStorage<T>(key: string, initialValue: T): [T, (value: T | ((val: T) => T)) => void] {
   // State to store our value
@@ -18,20 +18,21 @@ export function useLocalStorage<T>(key: string, initialValue: T): [T, (value: T 
   }, [key]);
 
   // Return a wrapped version of useState's setter function that persists the new value to localStorage.
-  const setValue = (value: T | ((val: T) => T)) => {
+  const setValue = useCallback((value: T | ((val: T) => T)) => {
     try {
-      // Allow value to be a function so we have same API as useState
-      const valueToStore = value instanceof Function ? value(storedValue) : value;
-      // Save state
-      setStoredValue(valueToStore);
-      // Save to local storage
-      if (typeof window !== "undefined") {
-        window.localStorage.setItem(key, JSON.stringify(valueToStore));
-      }
+      setStoredValue((prevValue) => {
+        // Allow value to be a function so we have same API as useState
+        const valueToStore = value instanceof Function ? value(prevValue) : value;
+        // Save to local storage
+        if (typeof window !== "undefined") {
+          window.localStorage.setItem(key, JSON.stringify(valueToStore));
+        }
+        return valueToStore;
+      });
     } catch (error) {
       console.warn(`Error setting localStorage key "${key}":`, error);
     }
-  };
+  }, [key]);
 
   return [storedValue, setValue];
 }
